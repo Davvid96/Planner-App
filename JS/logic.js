e51a2cf4243d49944ccc1af71fcf7bf1a991613b
Davvid96 <zajacek19@gmail.com>
2023-01-27 21:02:15 +0000

    modified js to take out local storage data and insert into text field + added moment js to give time at top of page

--- JS/logic.js
+++ JS/logic.js
@@ -1,18 +1,23 @@
+$(".row").on("submit", function (event) {
+  event.preventDefault();
+  var calendarData = localStorage.getItem("calendarData")
+    ? JSON.parse(localStorage.getItem("calendarData"))
+    : {};
+  console.log(event.target.elements.textBoxClass.value);
+  calendarData[`time${event.target.dataset.time}`] =
+    event.target.elements.textBoxClass.value;
+  localStorage.setItem("calendarData", JSON.stringify(calendarData));
+});
 
-$(".row").on("submit", function(event){
-    event.preventDefault();
-    var calendarData = localStorage.getItem("calendarData")?JSON.parse(localStorage.getItem("calendarData")): {};
-console.log(event.target.elements.textBoxClass.value);
-calendarData[`time${event.target.dataset.time}`]=event.target.elements.textBoxClass.value;
-localStorage.setItem("calendarData", JSON.stringify(calendarData));
-})
-
-$(function(event){
-console.log("page loaded");
-var calendarData = localStorage.getItem("calendarData")?JSON.parse(localStorage.getItem("calendarData")): null;
-if (!calendarData) return;
-
-})
-
-
-
+$(function (event) {
+  $("#currentDay").text(moment().format('llll'));
+  var calendarData = localStorage.getItem("calendarData")
+    ? JSON.parse(localStorage.getItem("calendarData"))
+    : null;
+  if (!calendarData) return;
+  $(".row").each(function () {
+    $(this)
+      .find("textarea")
+      .text(calendarData[`time${$(this).data("time")}`]);
+  });
+});
